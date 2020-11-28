a7224e3313c611e7e2b301ced13c3e34b28f9245
Camila Augusta dos Santos <camilaaugustasantos@yahoo.com.br>
2020-11-28 13:23:47 -0300

    FolhaDePagamento3

--- script-folha-pagamento.js
+++ script-folha-pagamento.js
@@ -1,4 +1,4 @@
-//Solução 1
+//SOLUCAO 1
 var numFunc = parseInt(gets());
 var qHora = parseInt(gets());
 var vHora = parseFloat(gets());
@@ -6,12 +6,12 @@ var salario = parseFloat(qHora * vHora).toFixed(2);
 console.log("NUMBER = " + numFunc);
 console.log("SALARY = U$ " + salario);
 
-//Solução 2 - Resumido
+//SOLUCAO 2 - Resumido
 var numFunc = parseInt(gets());
 var salario = parseFloat(parseInt(gets()) * parseFloat(gets())).toFixed(2);
 console.log("NUMBER = " + numFunc);
 console.log("SALARY = U$ " + salario);
 
-//Solução 3 - Resumido
+//SOLUCAO 3 - Resumido
 console.log("NUMBER = " + parseInt(gets()));
 console.log("SALARY = U$ " + (parseFloat(parseInt(gets()) * parseFloat(gets())).toFixed(2)));
